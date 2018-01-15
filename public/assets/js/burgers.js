603e2b4c58e87e9a81584ed6b7ded54da6588187
Kendra Krzywicki <kendra.krzywicki@knights.ucf.edu>
2018-01-15 15:54:09 -0500

    clean up code

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -3,12 +3,12 @@ $(function(){
 	$(".devour-burger").on("click", function(event){
 		// Set id based on button data-id attribute
 		var id = $(this).data("id");
-		// Send the PUT request
+		// Send the PUT request to move burger to devoured list
 		$.ajax("/api/burgers/"+id, {
 			type: "PUT",
 			data: id
 		}).then(function(){
-			// Reload the webpage
+			// Reload the webpage after update to database
 			location.reload();
 		})
 	});
